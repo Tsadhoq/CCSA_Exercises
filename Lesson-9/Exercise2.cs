0957695038ca2b6623ad11380b2d10f69d91f59e
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-03-30 11:31:09 +0100

    Update Exercise2.cs

--- Lesson-9/Exercise2.cs
+++ Lesson-9/Exercise2.cs
@@ -1,53 +1,34 @@
 using System;
 
-namespace LessonTask1
+namespace LessonTask2
 {
     class Program
     {
         static void Main(string[] args)
         {
-  
-            Console.WriteLine("==========================");
-            Console.WriteLine("SOLVE A QUADRATIC EQUATION (ax² + bx + c = 0)");
-            Console.WriteLine("==========================");
 
-            // Input the value of a, b and c
-            Console.Write("Enter the value of a: ");
-            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("========================================");
+            Console.WriteLine("CALCULATE THE FACTORIAL OF AN INTEGER (n)");
+            Console.WriteLine("========================================");
 
-            Console.Write("Enter the value of b: ");
-            float b = float.Parse(Console.ReadLine());
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter the value of c: ");
-            float c = float.Parse(Console.ReadLine());
-
-            // Call the method and display the result
-            Console.WriteLine(QuadraticRoot(a, b, c)); 
-
+            Console.WriteLine("========================================");
+            Console.WriteLine(FactorialOfN(n));
         }
 
-        static string QuadraticRoot(float num1, float num2, float num3)
+        static string FactorialOfN(int num)
         {
-
-            // Quadratic formula
-            double calc = Math.Pow(num2, 2) - (4 * num1 * num3);
-
-            if (calc < 0)
+            int theNum = num;
+            int fac = 1;
+            while (num > 0)
             {
-                return "No real solutions.";
+                fac = fac * num;
+                num--;
             }
 
-            double numerator1 = -num2 + Math.Sqrt(calc);
-            double numerator2 = -num2 - Math.Sqrt(calc);
-            double denominator = 2 * num1;
-
-            double root1 = numerator1 / denominator;
-            double root2 = numerator2 / denominator;
-
-            string result = $"x = {root1} or {root2}";
-
-            // Return the result
-            return result;
+            return $"{theNum}! = {fac}";
         }
     }
 }
