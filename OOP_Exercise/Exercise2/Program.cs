c7148b99cc2260e0d76276ff0a842f699be7bc36
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-10 18:06:37 +0100

    Add files via upload

---
+++ OOP_Exercise/Exercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+/*Define a class Human with properties "first name" and "last name". 
+ * Define the class Student inheriting Human, which has the property "mark". 
+ * Define the class Worker inheriting Human with the property "wage" and "hours worked". 
+ * Implement a "calculate hourly wage" method, which calculates a worker's hourly pay rate based on wage and hours worked.
+ * Write the corresponding constructors and encapsulate all data in properties.
+*/
+namespace ConsoleApp2
+{
+    // Human class
+    class Human
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+    }
+    // Student class
+    class Student : Human
+    {
+
+    }
+    // Worker class
+    class Worker : Human
+    {
+        private string Wage { get; set; }
+        private string Hoursworked { get; set; }
+
+        // Method
+        public double CalculateHourlyWage(double wage, double hours)
+        {
+            double hourlyWage = wage / hours;
+            return hourlyWage;
+        }
+    }
+    // Main class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
